76abf351296e1e3a2a96a941325b8f7fcdfc47b3
meowdaline <47653115+meowdaline@users.noreply.github.com>
2023-08-22 21:57:22 +0800

    Update q1-control-flow.js

--- assignment/q1-control-flow.js
+++ assignment/q1-control-flow.js
@@ -4,7 +4,7 @@
     - If `env` is "DEV", no change to `databaseCredential`.
     - If `env` is "STAGE", modify `databaseCredential` to "stageuser:password".
     - If `env` is "PROD", modify `databaseCredential` to "produser:password"
-    
+
 */
 
 const env = "DEV"; // Toggle between these possible values "DEV" | "STAGE" | "PROD"
@@ -12,4 +12,21 @@ let databaseCredential = "devuser:password";
 
 // Task: Add code here
 
+switch (env) {
+  case "STAGE":
+    databaseCredential = "stageuser:password";
+    break;
+  case "PROD":
+    databaseCredential = "produser:password";
+    break;
+  default:
+    console.log("Development environment; No changes to database credential.")
+}
+
 console.log(`Database credential for environment ${env} is ${databaseCredential}`);
+
+
+
+// References:
+// https://www.w3schools.com/js/js_switch.asp
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
