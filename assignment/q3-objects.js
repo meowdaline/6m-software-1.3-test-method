4111e9a6e15d1e46d12af4cb89fec9f94c0e70b8
meowdaline <47653115+meowdaline@users.noreply.github.com>
2023-08-26 00:17:36 +0800

    Update q3-objects.js

--- assignment/q3-objects.js
+++ assignment/q3-objects.js
@@ -6,25 +6,41 @@
     - NLB3 -> Introduction to Software Testing
     - NLB4 -> How to be a Software Developer
 
-    Tips: 
+    Tips:
     - Use a for loop and utilizes the index variable as a running number.
     - Be sure got use google!
 */
 
 const bookIdArr = ["NLB1", "NLB2", "NLB3", "NLB4"];
 const bookTitle = ["Lord of the Rings", "Programming for Dummies", "Introduction to Software Testing", "How to be a Software Developer"];
-let booksObj = {}; 
+let booksObj = {};
 
 function convert(keyArr, valueArr){
-    
+
     // Add code here
     /*
         Tips:
-        - Step 1: Create a local scoped object literal.
+        - Step 1: Create a local scoped object literal. > object that lives/exists within the function only.
         - Step 2: Define a for-loop that run based on keyArr's length.
         - Step 3: Within the for-loop, add the key and value to the local scoped object.
         - Step 4: Write a return statement to return the object literal after the for-loop code block.
     */
+    
+    // for (let i=0; i < 4; i++) {
+    //   let key = bookIdArr[i];
+    //   let value = bookTitle[i];
+    //   booksObj[key] = value
+    // }
+    //  let key = bookIdArr[i];
+    //   let value = bookTitle[i];
+    keyArr.forEach((element, index) => {
+        booksObj[element] = valueArr[index];
+    
+    });
+    return booksObj;
+    
+
+
 }
 
 function printByKey(key){
